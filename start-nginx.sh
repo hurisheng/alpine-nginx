fa64faead9a32e1fa7b11460f3c94f23ad927413
hurisheng <hurisheng@gmail.com>
2019-01-02 20:41:27 +0800

    modify watch

--- start-nginx.sh
+++ start-nginx.sh
@@ -2,18 +2,10 @@
 
 nginx -c /etc/nginx/config/nginx.conf
 
-watch="/etc/nginx/ssl/*/*"
-
-oldcksum=$(cksum ${watch})
+watch="/etc/nginx/ssl"
 
 inotifywait -e modify,move,create,delete -mr --timefmt '%d/%m/%y %H:%M' --format '%T' \
 ${watch} | while read date time; do
-
-	newcksum=$(cksum ${watch})
-	if [ "${newcksum}" != "${oldcksum}" ]; then
 		echo "At ${time} on ${date}, SSL file update detected."
-		oldcksum=${newcksum}
 		nginx -s reload
-	fi
-
 done
